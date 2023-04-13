57436ae36d394024c76f74796d7af51f11294723
Lindsay <1332360122@qq.com>
2023-04-13 11:02:01 +0800

    修改打包

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+# 忽略错误
+set -e
+
+# 构建
+npm run build
+
+# 进入待发布的目录
+cd docs/.vitepress/dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果部署到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+git push -f git@github.com:LindSayCapet/LindSayCapet.github.io.git   master #提交到这个分支
+cd -
